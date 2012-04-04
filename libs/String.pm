af6e9641fadc815160a2b49cb09391edca366300
Alexey Kosarev <alex@homyaki.info>
2012-04-04 12:13:55 +0400

    Add templates engine

--- libs/String.pm
+++ libs/String.pm
@@ -2,12 +2,24 @@ package Homyaki::String;
 
 use strict;
 
-sub regex_utf {
-	my $string = shift;
-	my $regex  = shift;
+sub handle_template {
+	my $class = shift;
 
-	
+	my %h = @_;
+	my $template_path = $h{template_path};
+	my $parameters    = $h{parameters} || {};
 
+	my $result = '';
+	if (open TEMPLATE, "<$template_path"){
+		while (my $string = <TEMPLATE>) {
+			foreach my $parameter (keys %{$parameters}){
+				$string =~ s/\%$parameter\%/$parameters->{$parameter}/g;
+				$result .= $string;
+			}
+		}
+	}
 
 	return $result;
 }
+
+1;
